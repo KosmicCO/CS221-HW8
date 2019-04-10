2ceb3a73afd0bbe19e8c3583cee88da3737fb899
Cruz <cruzbarnum@gmail.com>
2019-04-10 07:57:19 +0000

    Fixed encoder for the most part; however, havent figured out how to fix the args

--- encoder.cc
+++ encoder.cc
@@ -1,58 +1,77 @@
 #include "encoder.hh"
 #include <assert.h>
 
-Encoder::Encoder(std::string file)
-{
-    input_file_(file, ios::binary);
-    output_file_(file + ".comp", ios::binary);
+Encoder::Encoder(const char * file) {
+    input_file_ = new std::ifstream();
+    output_file_ = new std::ofstream();
 
-    assert(input_file_.open());
-    assert(output_file_.open());
+    input_file_->open(file);
+    output_file_->open("enc.txt.comp");
+
+    assert(* input_file_);
+    assert(* output_file_);
 
     eof_reached_ = false;
-    writer_(&output_file_, nullptr);
+    writer_ = new BitIO(output_file_, nullptr);
 
-    huffman_encoder_();   
+    huffman_encoder_ = new Huffman();   
 }
 
 Encoder::~Encoder()
 {
     if(input_file_){
-        input_file_.close();
+        input_file_->close();
     }
 
     if(output_file_){
-        output_file_.close();
+        output_file_->close();
     }
+    
+    delete writer_;
+    delete input_file_;
+    delete output_file_;
 }
 
 int Encoder::read_symbol()
 {
     assert(!eof_reached_);
     
-    if(input_file_.eof()){
+    if(input_file_->eof()){
         eof_reached_ = true;
         return Huffman::HEOF;
     }
 
-    return input_file_.get();
+    return input_file_->get();
 }
 
 void Encoder::write_bits(Huffman::bits_t bits)
 {
     for(bool b : bits){
-        writer_.output_bit(b);
+        writer_->output_bit(b);
     }
 }
 
 void Encoder::encode()
 {
     while(!eof_reached_){
-        write_bits(huffman_encoder_.encode(read_symbol())); // write the encoded inputs
+        write_bits(huffman_encoder_->encode(read_symbol())); // write the encoded inputs
     }
 }
 
+int main(int argc, const char ** argv)
+{
 
+
+    if(false) { //argc != 2){
+        std::cerr << "Needs two command arguments";
+        std::cout << argc << argv;
+        std::cout.flush();
+        assert(false);
+    }
+
+    Encoder encode("enc");
+    encode.encode();
+}
 
 
 
--- encoder.hh
+++ encoder.hh
@@ -7,22 +7,9 @@
 
 class Encoder {
 
-  private:
-
-    std::ifstream input_file_;
-    std::ofstream output_file_;
-
-    bool eof_reached_;
-    BitIO writer_;
-
-    Huffman huffman_encoder_;
-
-    int read_symbol();                  // returns Huffman::HEOF when done;
-    void write_bits(Huffman::bits_t);   // writes the bits to the file
-
   public:
 
-    Encoder(std::string);
+    Encoder(const char * file);
     ~Encoder();
 
     Encoder(const Encoder&) = delete;
@@ -32,26 +19,16 @@ class Encoder {
 
     void encode();
 
-}
+  private:
 
-// this is not supposed to be here and is only here for reference
+    std::ifstream * input_file_;
+    std::ofstream * output_file_;
 
-int main(int argc, char ** argv){
+    bool eof_reached_;
+    BitIO * writer_;
 
-    assert(argc == 1);
+    Huffman * huffman_encoder_;
 
-    std::string name_base(argv[0]);
-
-    std::ifstream input_file(name_base);
-    std::ofstream output_file{name_base + ".comp" };
-
-    BitIO reader(nullptr, &input_file);
-    BitIO writer(&output_file, nullptr);
-
-    
-    bool bb = reader.input_bit();
-    writer.output_bit(bb);
-
-    input_file.close();
-    output_file.close();
-}
+    int read_symbol();                  // returns Huffman::HEOF when done
+    void write_bits(Huffman::bits_t bits);   // writes the bits to the file
+};
